764ce6342af8e5af6f29bcfb9f59583b3bbf42fc
Adrian Par <adrian.par@novare.com.hk>
2023-05-13 01:29:37 +0800

    Initial commit todolist react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  // Add new todo button func
+  const handleAddTodo = () => {
+    if (!newTodo) return;
+    setTodos([...todos,{text: newTodo, completed: false}]);
+    setNewTodo('');
+  }
+
+   // /Delete button func
+   const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  //toggle the checkbox opposite of its property or value func
+  const handleToggle = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+ 
+  //Show or hide completed list Button 
+  const handleFilter = () => {
+    setShowCompleted(!showCompleted);
+  }
+
+  //filter and sort out the completed todo list func
+const filteredTodos = todos.sort((a, b) => a.completed - b.completed).filter(todo => showCompleted || !todo.completed);
+
+const checkedTodosCount = todos.filter((t) => t.completed).length;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+
+      <div>
+        {/* Textbox */}
+        <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+        &nbsp;&nbsp;&nbsp;
+
+        {/* Add A Task */}
+        <button onClick={handleAddTodo}>Add Todo</button>
+        &nbsp;&nbsp;&nbsp;
+        
+        {/* Clear Checklist */}
+        <button onClick={() => {setTodos([]);}}>Clear Todos</button>
+        &nbsp;&nbsp;&nbsp;
+        
+        {/* Show/Hide completed task*/}
+        <button onClick={handleFilter}>
+          {showCompleted ? 'Hide Completed Tasks' : 'Show Completed Tasks'}
+        </button>
+      </div>
+      <p>Checked Todos Count: {checkedTodosCount}</p>
+      <div>
+        {/* Todo Checklist */}
+      <ul>
+        {filteredTodos.map((todo, i) => (
+          <div  key={i}>
+            <input 
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleToggle(i)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</span>
+            &nbsp;&nbsp;&nbsp;
+
+            {/* Delete button */}
+            <button onClick={() => handleDelete(i)}>X</button>
+          </div>
+        ))}
+      </ul>
+      </div>
+      
     </div>
   );
 }
 
-export default App;
+export default TodoList;
